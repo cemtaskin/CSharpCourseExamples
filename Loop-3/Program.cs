95c088aad54532e021ed5c46dd8c266c00e6716f
cemtaskin <cemtaskin82@gmail.com>
2023-11-16 21:15:28 +0300

    foreach llop

---
+++ Loop-3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> names = new List<string>() { 
+                "Aslı",
+                "Berkay",
+                "Mehmet Berke",
+                "Ogulcan",
+                "Sadık",
+                "Selin"
+                
+            };
+
+            foreach(String name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
